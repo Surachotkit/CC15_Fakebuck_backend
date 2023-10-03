03a79d41fad4c59fa1e31f61cfd08c7dd0a106e5
Surachotkit <surachot.kit@gmail.com>
2023-10-03 18:03:37 +0700

    add photo by multer

---
+++ src/middlewares/upload.js
@@ -0,0 +1,17 @@
+const multer = require("multer");
+
+// เก็บไฟล์ไว้ไหน  และไฟล์ชื่ออะไร  //รับ parameter เป็น obj
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public");
+  },
+  // ชื่อไฟล์ date.now  อัพเดทเรื่อยๆ เวลา ณ ปัจจุบัน
+  filename: (req, file, cb) => {
+    console.log(file)
+    const split = file.originalname.split('.')
+    cb(null, '' + Date.now()+ Math.round(Math.random() * 1000000) + '.' + split[split.length - 1]);
+  },
+});
+
+const update = multer({ storage: storage });
+module.exports = update
